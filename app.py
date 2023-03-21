4daa37d514cab9ae8ea945f7d2dbee1ee9cef564
leo-usa <56208994+leo-usa@users.noreply.github.com>
2023-03-21 14:11:35 -0700

    Update app.py
    Changed the title and texts

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from utils import parse_pdf, embed_text, get_answer
 
-st.header("Doc QA")
-uploaded_file = st.file_uploader("Upload a pdf", type=["pdf"])
+st.header("ChatDoc")
+uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
 
 if uploaded_file is not None:
     index = embed_text(parse_pdf(uploaded_file))
-    query = st.text_area("Ask a question about the document")
+    query = st.text_area("Ask a question about the document, our ChatGPT will answer questions based on the document")
     button = st.button("Submit")
     if button:
         st.write(get_answer(index, query))
