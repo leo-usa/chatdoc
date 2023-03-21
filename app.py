a567dab99a4df76eaf4a3f24d39cbeaa0a2dd3a7
leo-usa <56208994+leo-usa@users.noreply.github.com>
2023-03-21 14:23:42 -0700

    Update app.py
    Some customizations: add page title, remove Streamlit header and footer.

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 from utils import parse_pdf, embed_text, get_answer
 
+hide_default_format = """
+       <style>
+       #MainMenu {visibility: hidden; }
+       footer {visibility: hidden;}
+       </style>
+       """
+st.markdown(hide_default_format, unsafe_allow_html=True)
+
+st.set_page_config(page_title="ChatDoc - The AI Bot Answering Your Questions based on a Document")
 st.header("ChatDoc - The AI Bot Answering Your Questions based on a Document")
 uploaded_file = st.file_uploader("Upload a PDF file, then you can ask questions, our ChatGPT will answer questions based on the document", type=["pdf"])
 
