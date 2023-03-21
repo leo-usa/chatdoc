36928b8ce5150027077281a802d964901b50e197
leo-usa <56208994+leo-usa@users.noreply.github.com>
2023-03-21 14:16:09 -0700

    Update app.py
    Changed the title and texts

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from utils import parse_pdf, embed_text, get_answer
 
-st.header("ChatDoc")
-uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
+st.header("ChatDoc - The AI Bot Answering Your Questions based on a Document")
+uploaded_file = st.file_uploader("Upload a PDF file, then you can ask questions, our ChatGPT will answer questions based on the document", type=["pdf"])
 
 if uploaded_file is not None:
     index = embed_text(parse_pdf(uploaded_file))
-    query = st.text_area("Ask a question about the document, our ChatGPT will answer questions based on the document")
+    query = st.text_area("Ask a question about the document")
     button = st.button("Submit")
     if button:
         st.write(get_answer(index, query))
